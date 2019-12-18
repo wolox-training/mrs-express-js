038395b4442c8244fa017696345d346bf85a9ac9
Marcosnr97 <marcos.rios@wolox.com.ar>
2019-12-18 09:40:40 -0300

    Added user model and user model migration

---
+++ app/models/user.js
@@ -0,0 +1,30 @@
+const sequelize = require('sequelize');
+
+module.exports = DataTypes => {
+  const User = sequelize.define(
+    'User',
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'first_name'
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'last_name'
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    { underscored: true }
+  );
+  return User;
+};
