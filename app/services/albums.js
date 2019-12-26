c6f95b8ba9943c38d3a23dd1fb2c0e84f7b74c72
Marcosnr97 <marcos.rios@wolox.com.ar>
2019-12-26 11:13:08 -0300

    pr fixes

--- app/services/albums.js
+++ app/services/albums.js
@@ -1,9 +1,9 @@
-const request_promise = require('request-promise');
+const requestPromise = require('request-promise');
 const { logger } = require('express-wolox-logger');
-const api_url = require('../../config').common.albumsApiURL;
+const apiUrl = require('../../config').common.albumsApiURL;
 
 const options = {
-  uri: `${api_url}`,
+  uri: `${apiUrl}`,
   headers: {
     'User-Agent': 'Request-Promise'
   },
@@ -11,8 +11,17 @@ const options = {
 };
 
 exports.getAlbums = id => {
-  if (id) options.uri = `${api_url}/${id}/photos`;
-  request_promise(options)
+  if (id) options.uri = `${apiUrl}/${id}`;
+  requestPromise(options)
+    .then(logger.info)
+    .catch(error => {
+      logger.error(error.message);
+    });
+};
+
+exports.getAlbumPhotos = id => {
+  options.uri = `${apiUrl}/${id}/photos`;
+  requestPromise(options)
     .then(logger.info)
     .catch(error => {
       logger.error(error.message);
